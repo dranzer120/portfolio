77762226b28091ee516a7f819b3d0c7f21f2da36
Jason Tong <jasontong818@gmail.com>
2017-09-25 19:05:03 -0700

    Update

--- server.js
+++ server.js
@@ -28,30 +28,7 @@ app.get('/contact', (req, res) => {
 
 app.post('/thanks', (req, res) => {
     res.render('thanks', { contact: req.body})
-});
-    //console.log(req.body);
-//     axios.put('https://sheets.googleapis.com/v4/spreadsheets/1fQp3TplBvN_hVoQ6BHf9imSDfFh040u6fK7YIkD6g6A/values:batchUpdate', 
-//     {
-//         "range": "Sheet1!A1:D5",
-//         "majorDimension": "ROWS",
-//         "values": [
-//             ["Item", "Cost", "Stocked", "Ship Date"],
-//             ["Wheel", "$20.50", "4", "3/1/2016"],
-//             ["Door", "$15", "2", "3/15/2016"],
-//             ["Engine", "$100", "1", "30/20/2016"],
-//             ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
-//         ]
-//     }).then(function(response){
-//         console.log(response)
-//         res.send('success')
-//     }).catch(function(err){
-//         console.log(err)
-//         res.send(err.message)
-//     })
-    
-//     //res.render('thanks', { contact: req.body})
-// });
-
+})
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
